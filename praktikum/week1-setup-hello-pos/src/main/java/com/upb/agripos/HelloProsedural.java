1b936546d92654aa14c1c702fdbd588ee1a251e1
rossaaqilaz-hash <rossaaqilaz@gmail.com>
2025-10-13 15:39:01 +0700

    Add files via upload

---
+++ praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloProsedural.java
@@ -0,0 +1,10 @@
+// Paradigma Prosedural
+public class HelloProsedural {
+    public static void main(String[] args) {
+        tampilPesan();
+    }
+
+    static void tampilPesan() {
+        System.out.println("Hello World, I am Rossa Aqila Zahra - 123456789");
+    }
+}
